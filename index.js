aec58befe0fb466b70ee548e3e847b4d4ccd9023
Kavachi Ukegbu <michelle.ukegbu@gmail.com>
2020-03-27 04:32:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string) {
   if(string == "I love you, Grandma.") {
     return "I love you,too."
   } else if(string == string.toLowerCase()){
-    return "I can'/t hear you!"
+    return "I can't hear you!"
     else if (string == string.toUpperCase()){
     return "YES INDEED!"
 
