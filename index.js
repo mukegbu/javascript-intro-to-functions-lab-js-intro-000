8de2ef518b45f613f0d42b5f96214623159e56dc
Kavachi Ukegbu <michelle.ukegbu@gmail.com>
2020-03-27 04:02:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ function shout(string) {
 }
 function whisper(string) {
 return string.toLowerCase()
-
 }
 function logShout(string) {
 console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+console.log(string.toLowerCase())
 }
